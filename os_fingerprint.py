dcb0d3d465aaca18a3e4d3c693a8f7290f3ee1f4
flaviasalutari <flavia.salutari@gmail.com>
2017-03-31 17:59:49 +0200

    Add files via upload

---
+++ os_fingerprint.py
@@ -0,0 +1,18 @@
+
+from scapy.all import *
+
+def os_finger(target):
+    ip = IP()
+    ping = ICMP()
+    ip.dst = target
+    send = sr1(ip/ping)
+    if send.ttl < 65:
+            print("IP:%s: Linux"%(target))
+
+    else:
+            print("IP:%s: Windows"%(target))
+
+
+if __name__ == '__main__':
+    target = raw_input("Type the IP: ")
+    os_finger(target)
